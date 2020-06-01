6f9a32403fda1f9b77e476c73b34ce7219880fb6
Clifford Yan <clifford.syan@gmail.com>
2020-06-01 17:54:04 -0400

    Prepare for initial Heroku push

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'static_pages#root'
 
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:create]
-    resource :session, only: [:destroy, :create]
+    resources :users
+    resource :session, only: [:destroy, :create, :new]
   end
 end
---
+++ db/migrate/20200601183014_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :email, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :gender, null: false, :default => 'm'
+      t.integer :weight, null: false
+      t.integer :height, null: false
+      t.integer :age, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.timestamps
+    end
+    add_index :users, [:username, :email, :session_token], unique: true
+  end
+end
