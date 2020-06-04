48671a6cd34728cbfe63410221fc483576a6c71f
Clifford Yan <clifford.syan@gmail.com>
2020-06-04 09:21:56 -0400

    Initial commit on walk tracking branch

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -4,6 +4,7 @@ import SignUpFormContainer from './session_form/signup_form_container';
 import NavBarContainer from './nav_bar/nav_bar_container';
 import ProfileContainer from './profile/profile_container';
 import SplashContainer from './splash/splash_container';
+import NavJrContainer from './nav_jr/nav_jr_container';
 
 import { 
   AuthRoute,
@@ -21,6 +22,7 @@ const App = () => (
     <AuthRoute exact path='/login' component={LogInFormContainer} />
     <AuthRoute exact path='/signup' component={SignUpFormContainer} />
     <ProtectedRoute exact path='/profile' component={ProfileContainer} />
+    <ProtectedRoute path='/profile' component={NavJrContainer} />
   </div>
 );
 
---
+++ frontend/components/nav_jr/nav_jr.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NavJr extends React.Component {
+    render(){
+        return (
+            <div className='nav-jr'>
+                
+            </div>
+        )
+    }
+}
+
+export default NavJr;
